43e38caab6a7fb9515801b58c84ef83d0cb85289
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2026-05-28 23:47:45 +0700

    Env injection works!
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- src/root.zig
+++ src/root.zig
@@ -15,9 +15,12 @@ const Shell = enum {
     fish,
 };
 
-// TODO: env support.
-// TODO: run with shell.
-pub fn runStep(allocator: std.mem.Allocator, io: std.Io, shell: Shell, command: []const u8) !std.process.RunResult {
+/// Run a command in the step.
+///
+/// Returns a result from the command.
+pub fn runStepCommand(allocator: std.mem.Allocator, io: std.Io, shell: Shell, command: []const u8, env: *std.process.Environ.Map) !std.process.RunResult {
+    // TODO: env support.
+    // TODO: run with shell.
     _ = shell; // autofix
 
     // We don't support stdin in any case.
@@ -26,18 +29,39 @@ pub fn runStep(allocator: std.mem.Allocator, io: std.Io, shell: Shell, command: 
         io,
         .{
             .argv = &[_][]const u8{ "bash", "-c", command },
+            .environ_map = env,
         },
     );
 }
 
-test runStep {
+test runStepCommand {
     const io = std.testing.io;
     const allocator = std.testing.allocator;
 
-    const result = try runStep(allocator, io, .bash, "echo 'hello, world'");
+    var env = std.process.Environ.Map.init(allocator);
+    defer env.deinit();
+
+    const result = try runStepCommand(allocator, io, .bash, "echo 'hello, world'", &env);
     defer allocator.free(result.stdout);
     defer allocator.free(result.stderr);
 
     try std.testing.expectEqual(0, result.term.exited);
     try std.testing.expectEqualStrings("hello, world\n", result.stdout);
+}
+
+test "runStepCommand - injecting the environment variables" {
+    const io = std.testing.io;
+    const allocator = std.testing.allocator;
+
+    var env = std.process.Environ.Map.init(allocator);
+    defer env.deinit();
+
+    try env.put("VAR", "VALUE");
+
+    const result = try runStepCommand(allocator, io, .bash, "echo $VAR", &env);
+    defer allocator.free(result.stdout);
+    defer allocator.free(result.stderr);
+
+    try std.testing.expectEqual(0, result.term.exited);
+    try std.testing.expectEqualStrings("VALUE\n", result.stdout);
 }
